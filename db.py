e2619ca88d2fc83d73e1e69fea60b265b15e8239
orangeBodhi <valentine.orange.petrov@gmail.com>
2025-06-10 17:23:44 +0300

    Update db.py

--- db.py
+++ db.py
@@ -61,8 +61,7 @@ def add_pushups(user_id, count):
     if not user:
         return False
     pushups_today = user["pushups_today"] + count
-    if pushups_today > 100:
-        return False
+    # Убрано ограничение на 100 — теперь можно превысить сотню!
     with get_connection() as conn:
         c = conn.cursor()
         c.execute("UPDATE users SET pushups_today=? WHERE user_id=?", (pushups_today, user_id))
